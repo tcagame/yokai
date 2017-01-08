cfb19d81ead40e9c2738240610e7791e4684a82a
okuty <okuty.phone@gmail.com>
2017-01-09 00:54:42 +0900

    #2261 新規プロジェクト作成

---
+++ Solution/main.cpp
@@ -0,0 +1,14 @@
+#include "Application.h"
+#include "Drawer.h"
+#include "Keyboard.h"
+
+void main ( ) {
+	TaskPtr drawer = DrawerPtr( new Drawer( "Resources" ) );
+	TaskPtr keyboard = KeyboardPtr( new Keyboard ); 
+
+	ApplicationPtr app = Application::getInstance( );
+	app->setWindowSize( 1280, 720 );
+
+	app->addTask( Drawer::getTag( ), drawer );
+	app->addTask( Keyboard::getTag( ), keyboard );
+}
