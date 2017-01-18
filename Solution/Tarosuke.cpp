d5f51c4397f40d31249eb34eb9d5152c7baeb6ec
okuty <okuty.phone@gmail.com>
2017-01-18 11:05:00 +0900

    たろすけ基本動作実装

--- Solution/Tarosuke.cpp
+++ Solution/Tarosuke.cpp
@@ -4,8 +4,8 @@
 #include "PsychicMgr.h"
 
 static const int MAX_TAROSUKE_CHIP_NUM = 101;
-static const int JUMP_COUNT = 3;
-static const int JUMP_POWER = 18;
+static const int JUMP_COUNT = 10;
+static const int JUMP_POWER = 20;
 static const int START_X = 400;
 static const int START_Y = 200;
 static const int JUNP_PATTERN  = 2;
@@ -13,10 +13,16 @@ static const int WALK_PATTERN = 8;
 static const int WAIT_TIME = 30;
 static const int CHIP_SIZE = 34 * 6;
 static const int CHIP_FOOT = 25;
+static const int MAX_SPEED = 15;
+static const int ACCEL_SPEED = 2;
+static const int BRAKE_SPEED = 6;
+static const int CAPACITY_SAVING_POWER = 60;
 
 Tarosuke::Tarosuke( PsychicMgrPtr psychic ) : 
 Character( START_X, START_Y, GRAPH_CHARACTER, CHIP_SIZE, CHIP_FOOT ) {
 	_psychic_mgr = psychic;
+	_jump_count = 0;
+	_action = ACTION_FLOAT;
 }
 
 Tarosuke::~Tarosuke( ) {
@@ -34,8 +40,14 @@ void Tarosuke::act( ) {
 	case ACTION_FLOAT:
 		actOnFloating( );
 		break;
-	case ACTION_SAVE:
-		actOnSaving( );
+	case ACTION_BRAKE:
+		actOnBraking( );
+		break;
+	case ACTION_SHOOT:
+		actOnShooting( );
+		break;
+	case ACTION_BURST:
+		actOnBursting( );
 		break;
 	}
 	
@@ -43,7 +55,6 @@ void Tarosuke::act( ) {
 }
 
 void Tarosuke::actOnStanding( ) {
-	
 	DevicePtr device = Device::getTask( );
 
 	if ( !isStanding( ) ) {
@@ -51,49 +62,180 @@ void Tarosuke::actOnStanding( ) {
 		return;
 	}
 	
-	if ( device->getDirY( ) > 50 && abs( device->getDirX( ) ) < 10 ) {
-		_action = ACTION_SAVE;
-		return;
-	}
-	
 	_action = ACTION_STAND;
 	
-	setAccelX( device->getDirX( DEVICE_1 ) / 10 );
+	bool accel = false;
+	if ( device->getDirX( ) > 90 ) {
+		if ( getAccelX( ) < 0 ) {
+			_action = ACTION_BRAKE;
+		} else if ( getAccelX( ) < MAX_SPEED ) {
+			accel = true;
+			int ax = getAccelX( ) + ACCEL_SPEED;
+			if ( ax > MAX_SPEED ) {
+				ax = MAX_SPEED;
+			}
+			setAccelX( ax );
+		}
+	}
+	
+	if ( device->getDirX( ) < -90 ) {
+		if ( getAccelX( ) > 0 ) {
+			_action = ACTION_BRAKE;
+		} else if ( getAccelX( ) > -MAX_SPEED ) {
+			accel = true;
+			int ax = getAccelX( ) - ACCEL_SPEED;
+			if ( ax < -MAX_SPEED ) {
+				ax = -MAX_SPEED;
+			}
+			setAccelX( ax );
+		}
+	}
 
-	if ( device->getPush( DEVICE_1 ) == BUTTON_C ) {
+
+	if ( device->getPush( ) == BUTTON_C ) {
+		_jump_count = JUMP_COUNT;
 		_action = ACTION_JUMP;
 		setAccelY( -JUMP_POWER );
 	}
+	
+	if ( device->getButton( ) == BUTTON_A ) {
+		_action = ACTION_SHOOT;
+	}
 
-	if ( device->getPush( ) == BUTTON_A  ) {
-		_action = ACTION_JUMP;
-		_psychic_mgr->shooting( getX( ), getY( ), true );
+	if ( !accel ) {
+		int ax = getAccelX( );
+
+		if ( ax < 0 ) {
+			ax += ACCEL_SPEED;
+			if ( ax > 0 ) {
+				ax = 0;
+			}
+		}
+		
+		if ( ax > 0 ) {
+			ax += -ACCEL_SPEED;
+			if ( ax < 0 ) {
+				ax = 0;
+			}
+		}
+
+		setAccelX( ax );
+		
+		if ( device->getDirY( ) > 90 ) {
+			_saving_power++;
+			if ( _saving_power >= CAPACITY_SAVING_POWER ) {
+				_action = ACTION_BURST;
+			}
+		} else {
+			_saving_power--;
+			if ( _saving_power < 0 ) {
+				_saving_power = 0;
+			}
+		}
 	}
 }
 
 void Tarosuke::actOnJumping( ) {
+	DevicePtr device = Device::getTask( );
+	
 	_action = ACTION_FLOAT;
+	if ( device->getButton( ) != BUTTON_C ) {
+		return;
+	}
+	
+	_jump_count--;
+	if ( _jump_count < 0 ) {
+		return;
+	}
+
+	_action = ACTION_JUMP;
+	setAccelY( -JUMP_POWER );
+}
+
+void Tarosuke::actOnBraking( ) {
+	int ax = getAccelX( );
+	
+	if ( ax < 0 ) {
+		ax += BRAKE_SPEED;
+		if ( ax > 0 ) {
+			ax = 0;
+		}
+	}
+		
+	if ( ax > 0 ) {
+		ax += -BRAKE_SPEED;
+		if ( ax < 0 ) {
+			ax = 0;
+		}
+	}
+
+	setAccelX( ax );
+
+	if ( ax == 0 ) {
+		_action = ACTION_STAND;
+	}
 }
 
 void Tarosuke::actOnFloating( ) {
+	DevicePtr device = Device::getTask( );
+
 	if ( isStanding( ) ) {
 		_action = ACTION_STAND;
 		return;
 	}
+
+	int ax = getAccelX( );
+
+	bool push = false;
+	if ( device->getDirX( ) > 90 ) {
+		if ( MAX_SPEED > ax ) {
+			ax += ACCEL_SPEED;
+		}
+		push = true;
+	}
+	
+	if ( device->getDirX( ) < -90 ) {
+		if ( -MAX_SPEED < ax ) {
+			ax += -ACCEL_SPEED;
+		}
+		push = true;
+	}
+
+	if ( !push ) {
+		if ( ax > 0 ) { 
+			ax += -ACCEL_SPEED / 2;
+		}
+
+		if ( ax < 0 ) { 
+			ax += ACCEL_SPEED / 2;
+		}
+	}
+
+	setAccelX( ax );
 }
 
-void Tarosuke::actOnSaving( ) {
-	DevicePtr device = Device::getTask( );
-	if ( device->getDirY( ) < 50 ) {
+void Tarosuke::actOnBursting( ) {
+	_saving_power--;
+	if ( _saving_power <= 0 ) {
+		_saving_power = 0;
 		_action = ACTION_STAND;
 	}
+}
+
+void Tarosuke::actOnShooting( ) {
+	_saving_power = 0;
+	_action = ACTION_STAND;
 }
 
 void Tarosuke::updateChip( ) {
 	switch ( _action ) {
 	case ACTION_STAND:
 		if ( getAccelX( ) == 0 ) {
-			setChipUV( 0, 0 );
+			if ( _saving_power == 0 ) {
+				setChipUV( 0, 0 );
+			} else {
+				setChipUV( _saving_power / ( CAPACITY_SAVING_POWER / 7 ), 4 );
+			}
 		} else {
 			const int WALK[ WALK_PATTERN ] = { 1, 2, 1, 0, 3, 4, 3, 0 };
 			setChipUV( WALK[ ( getX( ) / WAIT_TIME ) % WALK_PATTERN ], 0 );
@@ -102,6 +244,16 @@ void Tarosuke::updateChip( ) {
 	case ACTION_JUMP:
 	case ACTION_FLOAT:
 		setChipUV( 5, 0 );
+		break;
+	case ACTION_SAVE:
+		setChipUV( _saving_power / ( CAPACITY_SAVING_POWER / 7 ), 4 );
+		break;
+	case ACTION_BURST:
+		if ( _saving_power > CAPACITY_SAVING_POWER * 2 / 3 ) {
+			setChipUV( 7, 4 );
+		} else {
+			setChipUV( 8 + _saving_power / 10 % 2, 4 );
+		}
 		break;
 	}
 }
--- Solution/Tarosuke.h
+++ Solution/Tarosuke.h
@@ -13,9 +13,12 @@ public:
 private:
 	enum ACTION {
 		ACTION_STAND,
+		ACTION_SHOOT,
+		ACTION_BRAKE,
 		ACTION_JUMP,
 		ACTION_FLOAT,
 		ACTION_SAVE,
+		ACTION_BURST,
 		ACTION_MAX
 	};
 private:
@@ -23,10 +26,13 @@ private:
 	void actOnStanding( );
 	void actOnJumping( );
 	void actOnFloating( );
-	void actOnSaving( );
+	void actOnBraking( );
+	void actOnBursting( );
+	void actOnShooting( );
 	void updateChip( );
 private:
 	ACTION _action;
 	PsychicMgrPtr _psychic_mgr;
 	int _jump_count;
+	int _saving_power;
 };
