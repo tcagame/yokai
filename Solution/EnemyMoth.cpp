e629aee1d07973cc180aa96cc0cdd5cbfe460b57
yamada <a>
2017-01-20 15:58:24 +0900

    エラー修正

--- Solution/EnemyMoth.cpp
+++ Solution/EnemyMoth.cpp
@@ -2,9 +2,11 @@
 
 static const int WAIT_ANIME_TIME = 20;
 static const int MOVE_SPEED = 1;
+static const int CHIP_SIZE = 128;
+static const int CHIP_FOOT = 0;
 
 EnemyMoth::EnemyMoth( EnemyStockPtr enemy_stock, int x, int y ) :
-Enemy( enemy_stock, x, y, GRAPH_ENEMY, false ),
+Enemy( enemy_stock, x, y, GRAPH_ENEMY, CHIP_SIZE, CHIP_FOOT, false ),
 _anime_count( 0 ) {
 }
 
@@ -18,7 +20,21 @@ void EnemyMoth::act( ) {
 }
 
 void EnemyMoth::actMove( ) {
-
+	int add_x = 0;
+	int add_y = 0;
+	if ( getX( ) % 4 == 0 ) {
+		add_x = 1;
+	}
+	if ( getX( ) % 4 == 1 ) {
+		add_y = -1;
+	}
+	if ( getX( ) % 4 == 2 ) {
+		add_x = -1;
+	}
+	if ( getX( ) % 4 == 3 ) {
+		add_y = 1;
+	}
+	setAccelX( MOVE_SPEED + add_x );
 }
 
 void EnemyMoth::updateChip( ) {
