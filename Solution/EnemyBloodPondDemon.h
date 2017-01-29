72a83effc81186a0dcb576508069b85a80d5e6e0
yamada <a>
2017-01-29 15:34:58 +0900

    血の池の鬼追加

--- Solution/EnemyBloodPondDemon.h
+++ Solution/EnemyBloodPondDemon.h
@@ -6,7 +6,17 @@ public:
 	EnemyBloodPondDemon( int x, int y );
 	virtual ~EnemyBloodPondDemon( );
 private:
+	enum ACTION {
+		ACTION_WAIT,
+		ACTION_ATTACK,
+		MAX_ACTION
+	};
+private:
 	void act( );
 	void updateChip( );
+	void updateChipAttack( );
+private:
+	ACTION _action;
+	int _act_count;
 };
 
