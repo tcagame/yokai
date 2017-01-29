1f0ec069b95fcda44da5b24847274ff75d1f3908
ito <i>
2017-01-29 13:06:36 +0900

    作業の途中です

--- Solution/EnemyTree.cpp
+++ Solution/EnemyTree.cpp
@@ -1,10 +1,12 @@
 #include "EnemyTree.h"
+#include "EnemyGrowface.h"
 
 static const int CHIP_SIZE = 384;
 static const int CHIP_FOOT = 10;
 static const int GRAPH_HEIGHT = 256;
 static const int HP  = 10;
 static const int POW = 3;
+static const int CREATE_COUNT_GRAW_FACE = 60;
 
 EnemyTree::EnemyTree( int x, int y ) :
 Enemy( x, y, CHIP_SIZE, CHIP_FOOT, true, HP, POW ) {
@@ -16,6 +18,12 @@ EnemyTree::~EnemyTree( ) {
 }
 
 void EnemyTree::act( ) {
+	_count++;
+	if ( _count % CREATE_COUNT_GRAW_FACE == 0 ) {
+		int x = getX( );
+		int y = getY( );
+		_stock->addEnemy( EnemyPtr( new EnemyGrowFace( x, y ) ) );
+	}
 }
 
 double EnemyTree::getOverlappedRadius( ) const {
