4fa0ae19b03e8ad352afba1bf74d0b12bf09181f
ito <i>
2017-01-30 21:43:09 +0900

    クラスEnemyNoNeckGhostを追加しました

---
+++ Solution/EnemyNoNeckGhost.cpp
@@ -0,0 +1,9 @@
+#include "EnemyNoNeckGhost.h"
+
+
+EnemyNoNeckGhost::EnemyNoNeckGhost( ) {
+}
+
+
+EnemyNoNeckGhost::~EnemyNoNeckGhost( ) {
+}
---
+++ Solution/EnemyNoNeckGhost.h
@@ -0,0 +1,7 @@
+#pragma once
+class EnemyNoNeckGhost {
+public:
+	EnemyNoNeckGhost( );
+	~EnemyNoNeckGhost( );
+};
+
