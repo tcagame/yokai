63b267dc0c0454ef78832d02b7140a2bd1f93626
yamada <a>
2017-01-30 12:40:37 +0900

    クラス追加

---
+++ Solution/EnemyOneEyesSnake.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Enemy.h"
+
+class EnemyOneEyesSnake : public Enemy {
+public:
+	EnemyOneEyesSnake( int x, int y );
+	virtual ~EnemyOneEyesSnake( );
+private:
+	void act( );
+};
