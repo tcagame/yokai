ccd57e7204104a6f30a5346837dd020c8309241d
kent <ace>
2017-01-31 16:14:55 +0900

    槍骸骨、青坊主

--- Solution/EnemySkeletonSpear.cpp
+++ Solution/EnemySkeletonSpear.cpp
@@ -4,7 +4,7 @@ static const int CHIP_SIZE = 128;
 static const int CHIP_FOOT = 0;
 static const int HP = 3;
 static const int POW = 6;
-static const int MOVE_SPEED = 10;
+static const int MOVE_SPEED = 7;
 static const int WAIT_ANIME_TIME = 14;
 
 EnemySkeletonSpear::EnemySkeletonSpear( int x, int y ) :
