fff64db40712b6b6b6af89bd570bf176578a446a
yamada <a>
2017-01-31 16:27:56 +0900

    顔と手/一つ目ヘビ/

--- Solution/EnemyOneEyesSnake.cpp
+++ Solution/EnemyOneEyesSnake.cpp
@@ -4,14 +4,12 @@ static const int CHIP_SIZE = 128;
 static const int CHIP_FOOT = 0;
 static const int HP = 2;
 static const int POW = 1;
-static const int WAIT_ANIME_TIME = 5;
+static const int WAIT_ANIME_TIME = 15;
 static const int MOVE_SPEED = 4;
 static const int JUMP_POWER = -30;
-static const int WAIT_JUMP_TIME = 40;
 
 EnemyOneEyesSnake::EnemyOneEyesSnake( int x, int y ) :
-Enemy( x, y, CHIP_SIZE, CHIP_FOOT, true, HP, POW ),
-_act_count( 0 ) {
+Enemy( x, y, CHIP_SIZE, CHIP_FOOT, true, HP, POW ) {
 }
 
 
@@ -21,13 +19,12 @@ EnemyOneEyesSnake::~EnemyOneEyesSnake( ) {
 void EnemyOneEyesSnake::act( ) {
 	actMove( );
 	updateChip( );
-	_act_count++;
 }
 
 void EnemyOneEyesSnake::actMove( ) {
 	setAccelX( -MOVE_SPEED );
 
-	if ( _act_count % WAIT_JUMP_TIME == 0 ) {
+	if ( isStanding( ) ) {
 		setAccelY( JUMP_POWER );
 	}
 }
@@ -35,7 +32,7 @@ void EnemyOneEyesSnake::actMove( ) {
 void EnemyOneEyesSnake::updateChip( ) {
 	const int ANIME[ ] = { 0, 1, 2, 3, 4, 5, 6 };
 	int anime_num = sizeof( ANIME ) / sizeof( ANIME[ 0 ] );
-	int u = ANIME[ _act_count / WAIT_ANIME_TIME % anime_num ];
+	int u = ANIME[ getX( ) / WAIT_ANIME_TIME % anime_num ];
 	int v = 4;
 	setChipGraph( GRAPH_ENEMY_NORMAL, u, v );
 }
