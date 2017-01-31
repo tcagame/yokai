958dc6cfdf50c0f83f6a8797109e17b065c694f8
kase <kasu>
2017-01-31 18:23:49 +0900

    毒気HP,POW変更

--- Solution/EnemyMiasmaGray.cpp
+++ Solution/EnemyMiasmaGray.cpp
@@ -6,7 +6,7 @@ static const int MOVE_SPEED = 1;
 static const int CHIP_SIZE = 128;
 static const int CHIP_FOOT = 0;
 static const int HP  = 3;
-static const int POW = 3;
+static const int POW = 6;
 static const int RANGE = 10;
 
 EnemyMiasmaGray::EnemyMiasmaGray( int x, int y ) :
