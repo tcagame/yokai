f52bd7b8634c80d3dafdcde34cc254ac31c60e83
yamada <a>
2017-01-31 10:03:35 +0900

    手の妖怪

--- Solution/EnemyHandMonster.cpp
+++ Solution/EnemyHandMonster.cpp
@@ -4,8 +4,8 @@ static const int CHIP_SIZE = 128;
 static const int CHIP_FOOT = 0;
 static const int HP = 1;
 static const int POW = 1;
-static const int WAIT_APPEAR_TIME = 30;
-static const int WAIT_ANIME_TIME = 4;
+static const int WAIT_APPEAR_TIME = 60;
+static const int WAIT_ANIME_TIME = 3;
 
 EnemyHandMonster::EnemyHandMonster( int x, int y ) :
 Enemy( x, y, CHIP_SIZE, CHIP_FOOT, true, HP, POW ),
@@ -36,13 +36,17 @@ void EnemyHandMonster::act( ) {
 void EnemyHandMonster::actWait( ) {
 	if ( _act_count > WAIT_APPEAR_TIME ) {
 		_action = ACTION_THROW;
+		_act_count = 0;
 	}
 }
 
 void EnemyHandMonster::actThrow( ) {
-	const int ANIME[ ] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	const int ANIME[ ] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 	int anime_num = sizeof( ANIME ) / sizeof( ANIME[ 0 ] );
 	int u = ANIME[ _act_count / WAIT_ANIME_TIME % anime_num ];
 	int v = 0;
 	setChipGraph( GRAPH_ENEMY_NOMAL, u, v );
+	if ( u == 0 ) {
+		setAccelY( -1000 );
+	}
 }
