7434847953642b7fa71a809a7946dfdc90a72f61
okuty <okuty.phone@gmail.com>
2017-02-02 02:57:38 +0900

    タイトルでタロ助がうごく

--- Solution/SceneTitle.cpp
+++ Solution/SceneTitle.cpp
@@ -6,6 +6,7 @@
 #include "Define.h"
 #include "Keyboard.h"
 #include "Game.h"
+#include <time.h>
 
 static const int TITLE_WIDTH  = 1024;
 static const int TITLE_HEIGHT = 256;
@@ -22,17 +23,28 @@ static const int FPC = 5;
 static const int SELECT_X = 600;
 static const int SELECT_Y = 500;
 static const int DEMO_COUNT = 600;
+static const int TAROSUKE_SPEED = 10;
+static const int TAROSUKE_ACCEL = 1;
 
 SceneTitle::SceneTitle( ) {
+	srand( ( unsigned int )time( NULL ) );
+
 	DrawerPtr drawer = Drawer::getTask( );
-	drawer->loadGraph( GRAPH_TITLE_FONT,  "title/Yokai_UI_title_font.png"  );
-	drawer->loadGraph( GRAPH_TITLE_FRAME, "title/Yokai_UI_title_frame.png" );
-	drawer->loadGraph( GRAPH_TITLE_PANEL, "title/Yokai_UI_title_panel.png" );
+	drawer->loadGraph( GRAPH_CHARACTER_1 , "Character/Character1.png");
+	drawer->loadGraph( GRAPH_TITLE_FONT  , "title/Yokai_UI_title_font.png"  );
+	drawer->loadGraph( GRAPH_TITLE_FRAME , "title/Yokai_UI_title_frame.png" );
+	drawer->loadGraph( GRAPH_TITLE_PANEL , "title/Yokai_UI_title_panel.png" );
 	drawer->loadGraph( GRAPH_PLAYERCHOICE, "title/status_player_choice.png");
-	drawer->loadGraph( GRAPH_CURSORY, "title/cursory.png" );
+	drawer->loadGraph( GRAPH_CURSOR      , "title/cursory.png" );
 
 	_count = 0;
+	_panel_count = 0;
 	_select = 0;
+	
+	_tarosuke_x = TITLE_CENTER_X;
+	_tarosuke_vx = 0;
+	_tarosuke_right = false;
+	_tarosuke_move = false;
 }
 
 SceneTitle::~SceneTitle( ) {
@@ -95,6 +107,7 @@ Scene::NEXT SceneTitle::update( ) {
 
 void SceneTitle::act( ) {
 	_count++;
+	_panel_count++;
 
 	GamePtr game = Game::getTask( );
 	if ( game->getFade( ) != Game::FADE_NONE ) {
@@ -111,9 +124,49 @@ void SceneTitle::act( ) {
 
 	if ( device->getDirY( ) < 0 ) {
 		_select = 0;
+		_count = 0;
 	}
 	if ( device->getDirY( ) > 0 ) {
 		_select = 1;
+		_count = 0;
+	}
+
+	if ( _count % 60 == 0 ) {
+		_tarosuke_move = ( rand( ) % 3 != 0 );
+		if ( _tarosuke_move ) {
+			_tarosuke_right = ( rand( ) % 2 == 0 );
+		}
+	}
+	
+	if ( _tarosuke_move ) {
+		if ( _tarosuke_right ) {
+			_tarosuke_vx += TAROSUKE_ACCEL;
+			if ( _tarosuke_vx > TAROSUKE_SPEED ) {
+				_tarosuke_vx = TAROSUKE_SPEED;
+			}
+		} else {
+			_tarosuke_vx += -TAROSUKE_ACCEL;
+			if ( _tarosuke_vx < -TAROSUKE_SPEED ) {
+				_tarosuke_vx = -TAROSUKE_SPEED;
+			}
+		}
+	} else {
+		if ( _tarosuke_vx > 0 ) {
+			_tarosuke_vx -= TAROSUKE_ACCEL;
+		}
+		if ( _tarosuke_vx < 0 ) {
+			_tarosuke_vx += TAROSUKE_ACCEL;
+		}
+	}
+
+	_tarosuke_x += _tarosuke_vx;
+	if ( _tarosuke_x < TITLE_CENTER_X - 400 ) {
+		_tarosuke_x = TITLE_CENTER_X - 400;
+		_tarosuke_vx = 0;
+	}
+	if ( _tarosuke_x > TITLE_CENTER_X + 400 ) {
+		_tarosuke_x = TITLE_CENTER_X + 400;
+		_tarosuke_vx = 0;
 	}
 }
 
@@ -124,6 +177,28 @@ void SceneTitle::draw( ) {
 		Drawer::Transform( TITLE_CENTER_X - TITLE_WIDTH / 2, TITLE_CENTER_Y - TITLE_HEIGHT / 2),
 		GRAPH_TITLE_FONT );
 	drawer->setSprite( sprite_font );
+	
+	{
+		int sy = TITLE_CENTER_Y - 32;
+		int sx1 = _tarosuke_x - 64;
+		int sx2 = sx1 + 128;
+		if ( _tarosuke_right ) {
+			int tmp = sx1;
+			sx1 = sx2;
+			sx2 = tmp;
+		}
+		const int ANIM[ 8 ] = { 0, 1, 2, 1, 0, 3, 4, 3 };
+		int pattern = ANIM[ _tarosuke_x / 20 % 8 ];
+		if ( pattern == 0 ) {
+			SoundPtr sound = Sound::getTask( );
+			sound->playSE( "yokai_voice_15.wav" );
+		}
+		int tx = pattern * 128;
+		Drawer::Sprite sprite( 
+			Drawer::Transform( sx1, sy, tx, 0, 128, 128, sx2, sy + 128 ),
+			GRAPH_CHARACTER_1 );
+		drawer->setSprite( sprite );
+	}
 
 	Drawer::Sprite sprite_frame( 
 		Drawer::Transform( TITLE_CENTER_X - TITLE_WIDTH / 2, TITLE_CENTER_Y - TITLE_HEIGHT / 2),
@@ -137,14 +212,14 @@ void SceneTitle::draw( ) {
 	
 	Drawer::Sprite sprite_cusory(
 		Drawer::Transform( CHOICE_CURSORY_X, CHOICE_CURSORY_Y + _select * 100 ),
-		GRAPH_CURSORY );
+		GRAPH_CURSOR );
 	drawer->setSprite( sprite_cusory );
 	
 
 	for ( int i = 0; i < 5; i++ ) {
 		int x = TITLE_CENTER_X - PANEL_PITCH * 2 + i * PANEL_PITCH - PANEL_WIDTH / 2;
 		int y = TITLE_CENTER_Y - PANEL_HEIGHT / 2;
-		int tx = abs( ( _count / FPC + i ) % 10 - 3 );
+		int tx = abs( ( _panel_count / FPC + i ) % 10 - 3 );
 		if ( tx > 3 ) {
 			tx = 3;
 		}
@@ -153,12 +228,5 @@ void SceneTitle::draw( ) {
 			Drawer::Transform( x, y, tx, 0, PANEL_WIDTH, PANEL_HEIGHT ),
 			GRAPH_TITLE_PANEL );
 		drawer->setSprite( sprite_panel);
-	}
-
-
-	GamePtr game = Game::getTask( );
-	Game::FADE fade = game->getFade( );
-	if ( fade == Game::FADE_NONE ) {
-		//drawer->drawString( 200, 200, "東京コミュニケーションアート専門学校" );
 	}
 }
--- Solution/SceneTitle.h
+++ Solution/SceneTitle.h
@@ -11,7 +11,12 @@ private:
 	void act( );
 	void draw( );
 private:
+	int _panel_count;
 	int _count;
 	int _select;
+	int _tarosuke_x;
+	int _tarosuke_vx;
+	bool _tarosuke_right;
+	bool _tarosuke_move;
 };
 
