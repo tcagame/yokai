1c66968a02e9dc8c4e828e91a5a4992bd30ff1e5
iwafune <a>
2017-02-02 14:06:25 +0900

    無敵時間を長くした　＋　中ボスエネミーのＰＯＷを上げた

--- Solution/EnemyTree.cpp
+++ Solution/EnemyTree.cpp
@@ -5,7 +5,7 @@ static const int CHIP_SIZE = 512;
 static const int CHIP_FOOT = 10;
 static const int GRAPH_HEIGHT = 256;
 static const int HP  = 15;
-static const int POW = 6;
+static const int POW = 10;
 static const int CREATE_COUNT_GRAW_FACE = 25;
 
 static const int OFFSET_X = -257;
