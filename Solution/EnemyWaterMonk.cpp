4bfd8f7959ae36ff80d4bb5ec9e0e0c6251e3be2
kiyotaka <kytk>
2017-02-14 18:21:27 +0900

    海坊主、槍骸骨修正

--- Solution/EnemyWaterMonk.cpp
+++ Solution/EnemyWaterMonk.cpp
@@ -20,6 +20,9 @@ void EnemyWaterMonk::act( ) {
 	const int ANIME[ ] = { 0, 1, 2, 3, 4, 3, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	int anime_num = sizeof( ANIME ) / sizeof( ANIME[ 0 ] );
 	_over_lapped_pos = ANIME[ _act_count / WAIT_ANIME_TIME % anime_num ];
+	if ( _over_lapped_pos == 4 ) {
+		_over_lapped_pos = 3;
+	}
 	int u = ANIME[ _act_count / WAIT_ANIME_TIME % anime_num ];
 	int v = 4;
 	setChipGraph( GRAPH_ENEMY_BIG, u, v );
